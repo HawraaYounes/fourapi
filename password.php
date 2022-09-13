70fecb9d7b3de4eb98e9f34826262a2b19c187f1
HawraaYounes <hawraayounes0@gmail.com>
2022-09-13 08:11:59 +0300

    Adding password api

---
+++ password.php
@@ -0,0 +1,15 @@
+<?php
+$password = $_GET['password'];
+ 
+$number = preg_match('@[0-9]@', $password);
+$uppercase = preg_match('@[A-Z]@', $password);
+$lowercase = preg_match('@[a-z]@', $password);
+$specialChars = preg_match('@[^\w]@', $password);
+ 
+if(strlen($password) < 12 || !$number || !$uppercase || !$lowercase || !$specialChars) {
+    $strong="Not Strong";
+} else {
+    $strong="Strong";
+}
+
+?>
