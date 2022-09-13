8e032131f0952ec1e6d2ceebb81936c7375b60a1
HawraaYounes <hawraayounes0@gmail.com>
2022-09-13 08:20:07 +0300

    commenting the code

--- calculationapi.php
+++ calculationapi.php
@@ -1,10 +1,10 @@
 <?php
 $a =$_POST["a"];
 $a =$_POST["b"];
-$c =$_POST["c"];
+$c =$_POST["c"];//take a,b,c values
 
 $result=[
-    "result"=>pow($a,3)+($b*$c)-($a/$b)
+    "result"=>pow($a,3)+($b*$c)-($a/$b)//return calculation result
 ];
 
 echo json_encode($result);
--- christmasapi.php
+++ christmasapi.php
@@ -1,14 +1,14 @@
 <?php
-$todaydate =$_GET["todaydate"];
+$todaydate =$_GET["todaydate"];//get today date 
 $today= new DateTime($todaydate);
-if(date("m") == 12 && date("d") > 25){
-    $christmasDay = (date("Y") + 1) . "-12-25";
+if(date("m") == 12 && date("d") > 25){//check if christmas of this year passed
+    $christmasDay = (date("Y") + 1) . "-12-25";//if yes,add 1 to year 
 }
 else{
-    $christmasDay = date("Y") ."-12-25";
+    $christmasDay = date("Y") ."-12-25";//if no, keep the current year
 }
 $christmasDay = new DateTime($christmasDay);
-$timeleft= $today->diff($christmasDay);
+$timeleft= $today->diff($christmasDay);//calculate the difference between today date and christmas
 $result=[
     "result"=>$timeleft->days ." days left for christmas"
 ];
